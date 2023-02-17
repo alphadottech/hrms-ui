865d1c1d87bf598c4d56488db404e5a932cd8a96
sanskritiadt <sanskritidhakad.adt@gmail.com>
2023-02-17 11:44:41 +0530

    files added

---
+++ src/src/protected.js
@@ -0,0 +1,10 @@
+// import { Navigate } from 'react-router-dom';
+// const Protected = (children) => {
+//     const token = localStorage.getItem("token");
+//     if (!token) {
+//         <Navigate to='/' replace />
+//     } 
+
+//         return children;
+// }
+// export default Protected;
